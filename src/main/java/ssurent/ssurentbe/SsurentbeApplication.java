93cd36d1763e97fcae605ecd8d320fde36dd8fd3
Crom <doyoung.yang2005@gmail.com>
2026-01-16 18:59:13 +0900

    Feat : 기본설정파일 올리기1

---
+++ src/main/java/ssurent/ssurentbe/SsurentbeApplication.java
@@ -0,0 +1,13 @@
+package ssurent.ssurentbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsurentbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsurentbeApplication.class, args);
+    }
+
+}
