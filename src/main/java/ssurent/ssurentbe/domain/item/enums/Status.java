5295e0ca63607c97f9d3f99f22179e7d45d62267
Todom2 <schoolpang@naver.com>
2026-01-25 21:08:50 +0900

    [Refactor] #2 Status 에 description 추가

--- src/main/java/ssurent/ssurentbe/domain/item/enums/Status.java
+++ src/main/java/ssurent/ssurentbe/domain/item/enums/Status.java
@@ -1,7 +1,14 @@
 package ssurent.ssurentbe.domain.item.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Status {
-    ACTIVE,
-    FIXING,
-    INACTIVE
+    ACTIVE("대여 가능"),
+    FIXING("수리 중"),
+    INACTIVE("대여 불가");
+
+    private String description;
 }
