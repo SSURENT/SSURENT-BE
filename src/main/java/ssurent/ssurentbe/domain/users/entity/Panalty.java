574f8ea9259a9bed74e9a871e4d4abb446bef9c8
Crom <doyoung.yang2005@gmail.com>
2026-01-29 04:23:42 +0900

    Feat #3 Id타입 String으로 변환 및 Auth 오류 수정

---
+++ src/main/java/ssurent/ssurentbe/domain/users/entity/Panalty.java
@@ -0,0 +1,22 @@
+package ssurent.ssurentbe.domain.users.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ssurent.ssurentbe.common.base.BaseEntity;
+import ssurent.ssurentbe.domain.users.enums.PanaltyTypes;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "panalty")
+public class Panalty extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "panalty_type")
+    private PanaltyTypes panaltyType;
+}
