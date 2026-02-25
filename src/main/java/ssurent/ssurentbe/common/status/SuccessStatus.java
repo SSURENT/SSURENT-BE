e98edd2c874ead58b6af36388d0d33239a2fb921
Crom <doyoung.yang2005@gmail.com>
2026-02-25 19:44:34 +0900

    [Feat] #23 상태코드 추가

--- src/main/java/ssurent/ssurentbe/common/status/SuccessStatus.java
+++ src/main/java/ssurent/ssurentbe/common/status/SuccessStatus.java
@@ -24,6 +24,7 @@ public enum SuccessStatus implements BaseStatus {
      *  Rental
      */
     RENTAL_CREATE_SUCCESS(HttpStatus.CREATED, "RENTAL_201", "물품 대여 성공"),
+    MY_RENTAL_SUCCESS(HttpStatus.OK, "RENTAL_200", "내 대여 목록 조회 성공"),
     RENTAL_HISTORY_SUCCESS(HttpStatus.OK, "RENTAL_200", "유저 대여 내역 조회 성공"),
 
     /**
