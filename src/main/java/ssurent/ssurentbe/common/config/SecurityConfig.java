d210679c838b8d8e719aa685dd35b06981407304
Crom <doyoung.yang2005@gmail.com>
2026-02-25 17:26:33 +0900

    [Fix] #14 admin경로 접근 권한 수정

--- src/main/java/ssurent/ssurentbe/common/config/SecurityConfig.java
+++ src/main/java/ssurent/ssurentbe/common/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/v1/admin/**").hasAnyRole("ADMIN", "SUPERADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
