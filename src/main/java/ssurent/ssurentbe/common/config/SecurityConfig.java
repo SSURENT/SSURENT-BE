0fcba9e4d44fbf34dd891b1b40882baefc6cf04d
Crom <doyoung.yang2005@gmail.com>
2026-02-25 17:11:13 +0900

    [Fix] #14 security 수정

--- src/main/java/ssurent/ssurentbe/common/config/SecurityConfig.java
+++ src/main/java/ssurent/ssurentbe/common/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/v1/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
