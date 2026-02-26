20ef846a8bfe23f0c4ed673ffcc45bba466e871d
Crom <doyoung.yang2005@gmail.com>
2026-02-26 21:12:35 +0900

    [Fix] #28 Redis 설정 변경  - setPassword()를 deprecated된 LettuceConnectionFactory 방식 대신 RedisStandaloneConfiguration에서 생성 시점에 설정하도록 변경

--- src/main/java/ssurent/ssurentbe/common/config/RedisConfig.java
+++ src/main/java/ssurent/ssurentbe/common/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -22,11 +23,11 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         if (password != null && !password.isBlank()) {
-            factory.setPassword(password);
+            config.setPassword(password);
         }
-        return factory;
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
