a31051847699907646afa0390ac8023343f2e84d
Crom <doyoung.yang2005@gmail.com>
2026-03-04 12:57:47 +0900

    [Fix] #32 Exception 처리

--- src/main/java/ssurent/ssurentbe/domain/rental/service/RentalQueryService.java
+++ src/main/java/ssurent/ssurentbe/domain/rental/service/RentalQueryService.java
@@ -88,6 +88,10 @@ public class RentalQueryService {
             LocalDate startDate,
             LocalDate endDate
     ) {
+        if (startDate == null && endDate == null){
+            throw new GeneralException(ErrorStatus.RENTAL_INVALID_DATE_RANGE);
+        }
+
         if (startDate.isAfter(endDate)) {
             throw new GeneralException(ErrorStatus.RENTAL_INVALID_DATE_RANGE);
         }
