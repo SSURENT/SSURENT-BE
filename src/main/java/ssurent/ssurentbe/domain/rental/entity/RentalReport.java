1632accd42cb447c5a77a83b1a6f8b522b3761e4
Crom <doyoung.yang2005@gmail.com>
2026-03-08 17:13:12 +0900

    [Feat] #39 레포트 엔티티 추가

---
+++ src/main/java/ssurent/ssurentbe/domain/rental/entity/RentalReport.java
@@ -0,0 +1,44 @@
+package ssurent.ssurentbe.domain.rental.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ssurent.ssurentbe.common.base.BaseEntity;
+import ssurent.ssurentbe.domain.rental.enums.ProblemType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "rental_report")
+public class RentalReport extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rental_id", nullable = false)
+    private RentalHistory rentalHistory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "problem_type", nullable = false)
+    private ProblemType problemType;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "reported_at", nullable = false)
+    private LocalDateTime reportedAt;
+
+    public static RentalReport of(RentalHistory rentalHistory, ProblemType problemType, String description) {
+        return RentalReport.builder()
+                .rentalHistory(rentalHistory)
+                .problemType(problemType)
+                .description(description)
+                .reportedAt(LocalDateTime.now())
+                .build();
+    }
+}
