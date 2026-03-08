eeffd7c89e21264d25e2a9ebe790a530241e1bcd
Crom <doyoung.yang2005@gmail.com>
2026-03-08 18:13:55 +0900

    [Feat] #39 리포지터리 추가

--- src/main/java/ssurent/ssurentbe/domain/rental/repository/RentalReportRepository.java
+++ src/main/java/ssurent/ssurentbe/domain/rental/repository/RentalReportRepository.java
@@ -3,7 +3,11 @@ package ssurent.ssurentbe.domain.rental.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ssurent.ssurentbe.domain.rental.entity.RentalReport;
 
+import java.util.List;
+
 public interface RentalReportRepository extends JpaRepository<RentalReport, Long> {
 
     long countByResolvedFalse();
+
+    List<RentalReport> findAllByResolvedFalse();
 }
