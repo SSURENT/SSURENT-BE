292b01cba0c6c9dfb5c8b08ad2ccf34cb4ce9ed4
Crom <doyoung.yang2005@gmail.com>
2026-03-22 15:03:22 +0900

    [Feat] #43 성공/실패 상태 추가

--- src/main/java/ssurent/ssurentbe/common/status/ErrorStatus.java
+++ src/main/java/ssurent/ssurentbe/common/status/ErrorStatus.java
@@ -35,6 +35,8 @@ public enum ErrorStatus implements BaseStatus {
     INVALID_VERIFICATION_CODE(HttpStatus.UNAUTHORIZED, "AUTH_401", "인증 코드가 일치하지 않습니다."),
     VERIFICATION_CODE_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_401", "인증 코드가 만료되었습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_401", "비밀번호가 올바르지 않습니다."),
+    INVALID_RESET_TOKEN(HttpStatus.BAD_REQUEST, "AUTH_400", "비밀번호 재설정 토큰이 유효하지 않습니다."),
+    SMS_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_500", "SMS 발송에 실패했습니다."),
 
     /**
      * JWT
