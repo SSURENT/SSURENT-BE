d67784584036ac21bb8a4e8c19d4a512f8085978
Crom <doyoung.yang2005@gmail.com>
2026-03-22 15:43:21 +0900

    [Feat] #43 sms 로깅 추가

--- src/main/java/ssurent/ssurentbe/common/auth/service/SmsService.java
+++ src/main/java/ssurent/ssurentbe/common/auth/service/SmsService.java
@@ -1,6 +1,7 @@
 package ssurent.ssurentbe.common.auth.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.service.DefaultMessageService;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import ssurent.ssurentbe.common.exception.GeneralException;
 import ssurent.ssurentbe.common.status.ErrorStatus;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SmsService {
@@ -21,12 +23,13 @@ public class SmsService {
     public void sendVerificationCode(String phoneNum, String code) {
         Message message = new Message();
         message.setFrom(senderNumber);
-        message.setTo(phoneNum);
+        message.setTo(phoneNum.replace("-", ""));
         message.setText("[SSURENT] 인증번호 [" + code + "]를 입력해주세요. (5분 이내 입력)");
 
         try {
             messageService.sendOne(new SingleMessageSendingRequest(message));
         } catch (Exception e) {
+            log.error("[SMS] 발송 실패 - to: {}, cause: {}", phoneNum.replace("-", ""), e.getMessage(), e);
             throw new GeneralException(ErrorStatus.SMS_SEND_FAILED);
         }
     }
