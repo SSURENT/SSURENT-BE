c3bf51cd3b9115a254c5cb127eebe201166d528c
Crom <doyoung.yang2005@gmail.com>
2026-03-22 15:12:29 +0900

    [Feat] #43 sms 전송용 비즈니스 로직 추가

---
+++ src/main/java/ssurent/ssurentbe/common/auth/service/SmsService.java
@@ -0,0 +1,33 @@
+package ssurent.ssurentbe.common.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ssurent.ssurentbe.common.exception.GeneralException;
+import ssurent.ssurentbe.common.status.ErrorStatus;
+
+@Service
+@RequiredArgsConstructor
+public class SmsService {
+
+    private final DefaultMessageService messageService;
+
+    @Value("${nurigo.sender-number}")
+    private String senderNumber;
+
+    public void sendVerificationCode(String phoneNum, String code) {
+        Message message = new Message();
+        message.setFrom(senderNumber);
+        message.setTo(phoneNum);
+        message.setText("[SSURENT] 인증번호 [" + code + "]를 입력해주세요. (5분 이내 입력)");
+
+        try {
+            messageService.sendOne(new SingleMessageSendingRequest(message));
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.SMS_SEND_FAILED);
+        }
+    }
+}
